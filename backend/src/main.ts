d33666ecaa84e4f44138d0eafe3fef8482372221
obidhunter-stack <obidhunter@gmail.com>
2025-12-23 19:38:54 +0000

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -85,7 +85,7 @@ async function bootstrap() {
   }
 
   // Start server
-  const port = Number(process.env.PORT) || 3000;
+  const port = Number(configService.get('PORT')) || 3000;
   await app.listen(port, '0.0.0.0');
 
   console.log(`
