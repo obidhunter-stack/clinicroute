8d8b5e323924f24a922d08def7fdaeeb95901a2c
obidhunter-stack <obidhunter@gmail.com>
2025-12-23 00:37:01 +0000

    Update health.controller.ts

--- backend/src/health.controller.ts
+++ backend/src/health.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { PrismaService } from '../config/prisma.service';
-import { Public } from '../common/decorators/public.decorator';
+import { PrismaService } from './config/prisma.service';
+import { Public } from './common/decorators/public.decorator';
 
 @ApiTags('Health')
 @Controller('health')
